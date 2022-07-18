a1203392bd9f2858f29bb8156b454293484e0d9c
tangxiaqiang <tangxiaqiang@outlook.com>
2022-07-19 07:51:24 +0800

    first run commit to backup

---
+++ launch.sh
@@ -0,0 +1,16 @@
+num_process=4
+g=4
+START_TIME=`date +%Y%m%d-%H:%M:%S`
+echo '[USAGE] sh train.sh <CONFIG> <GPU_NUM> <JOB_NAME>'
+mkdir -p logs
+LOG_FILE=logs/train-log-$START_TIME
+
+srun  -p caif_dev \
+      -n $num_process\
+      -x SH-IDC1-10-140-0-139 \
+      --gres=gpu:$g\
+      --ntasks-per-node=$g \
+      --job-name=multi_node \
+python  mytrain_ddp.py -m lanegcn \
+  2>&1 | tee $LOG_FILE 
+echo -e "\033[32m[ Please see LOG_FILE for details: \"tail -f ${LOG_FILE}\" ]\033[0m"
